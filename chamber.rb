38f3de89251716264fdfd7cb570ee199d8745110
Assaf Shomer <assafshomer@gmail.com>
2018-07-04 09:25:25 +0300

    initial commit

---
+++ chamber.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+
+require 'base64'
+require 'openssl'
+require 'fileutils'
+require './crypto_helper'
+include CryptoHelper
+
+puts "Encrypt or Decrypt? (E/D)"
+
+if gets.chomp =~ /\AE/ then
+  puts "Enter dir path"
+  dir_path = gets.chomp
+  puts "Enter Password (optional)"
+  password = gets.chomp
+  puts "Enter Iterations"
+  iterations = gets.chomp.to_i
+  enc_opts = { dir_path: dir_path }
+  enc_opts[:password] = password unless password.strip.empty?
+  enc_opts[:iterations] = iterations unless iterations.to_s.strip.empty?
+
+  encrypt_directory(enc_opts)
+else
+  puts "Enter zipped dir file name"
+  file_path = gets.chomp
+  puts "Enter Password or Key"
+  password_or_key = gets.chomp
+  puts "Enter Iterations"
+  iterations = gets.chomp.to_i
+  dec_opts = { file_path: file_path }
+  dec_opts[:password_or_key] = password_or_key unless password_or_key.strip.empty?
+  dec_opts[:iterations] = iterations unless iterations.to_s.strip.empty?
+
+  decrypt_directory(dec_opts)
+end
---
+++ crypto_helper.rb
@@ -0,0 +1,56 @@
+module CryptoHelper
+	require 'fileutils'
+	require './kingslayer.rb'
+	include Kingslayer
+	PBKDF2_ITERATIONS = 10
+	ENCRYPTED_FILE_SUFFIX = ".enc"
+	DECRYPTED_FILE_SUFFIX = ".dec"
+
+	def encrypt(opts = {})
+		password, file_path = opts[:password], opts[:file_path]
+		raise "Cannot encrypt #{file_path}. File does not exist" unless File.exist?(file_path)
+		params = { password: password, iter: opts[:iterations] || PBKDF2_ITERATIONS } if password
+		ks = Kingslayer::AES.new(params.to_h)
+		path = ks.encrypt_file(file_path, file_path + ENCRYPTED_FILE_SUFFIX)
+		puts "Your file was encrypted to #{path}"
+		puts "Your file was encrypted with #{ks.password}" unless password
+	end
+
+	def decrypt(opts = {})
+		password_or_key, file_path = opts[:password_or_key], opts[:file_path]
+		raise "Cannot decrypt #{file_path}. No password or key was provided" unless password_or_key
+		raise "Cannot decrypt #{file_path}. File does not exist" unless File.exist?(file_path)
+
+		begin
+			ks = Kingslayer::AES.new(password: password_or_key)
+			ks.decrypt_file(file_path, file_path + DECRYPTED_FILE_SUFFIX)
+		rescue
+			iterations = opts[:iterations] || PBKDF2_ITERATIONS
+			ks = Kingslayer::AES.new(password: password_or_key, iter: iterations)
+			ks.decrypt_file(file_path, file_path + DECRYPTED_FILE_SUFFIX)
+		end
+	end
+
+	def encrypt_directory(opts = {})
+		dir_path = opts[:dir_path]
+		raise "Cannot encrypt #{dir_path}. Directory does not exist" unless File.directory?(dir_path)
+		zipped_filename = dir_path.split("/").reject(&:empty?).join("-") + ".zip"
+		`zip -r #{zipped_filename} #{dir_path}`
+		encryption_options = { file_path: zipped_filename }
+		encryption_options[:password] ||= opts[:password]
+		encryption_options[:iterations] ||= opts[:iterations]
+		encrypt(encryption_options)
+		FileUtils.rm_rf dir_path
+		FileUtils.rm_f zipped_filename
+	end
+
+	def decrypt_directory(opts = {})
+		puts "decryption options are: #{opts}"
+		decrypted_file_path = decrypt(opts)
+		zipped_filename = decrypted_file_path.gsub(".enc.dec","")
+		FileUtils.mv(decrypted_file_path, zipped_filename)
+		`unzip #{zipped_filename}`
+		FileUtils.rm_f zipped_filename
+		FileUtils.rm_f opts[:file_path]
+	end
+end
